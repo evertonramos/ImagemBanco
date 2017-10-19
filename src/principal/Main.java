e9bf53cfae6b4a843a92b405d56c1ce55b3abf88
Everton A. Ramos <contato@everton.com.br>
2017-10-19 21:27:31 -0200

    .

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,5 +1,7 @@
 package principal;
 
+import java.sql.ResultSet;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("teste...");
@@ -7,8 +9,11 @@ public class Main {
         // faz a conexão ao banco
         PostgreSQL conexao = (new Conexao("nome do sistema")).getConexao();
         
+        ResultSet rs = conexao.query("select current_timestamp", new Object[0]);
         
-        
+        if(rs.next()) {
+            System.out.println(rs.getString(1));
+        }
         
         // finaliza a conexão
         conexao.disconnect();
