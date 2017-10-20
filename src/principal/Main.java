0dfe2c4df6458d230ee28caafe36b617c82e6ede
Everton A. Ramos <contato@everton.com.br>
2017-10-20 20:43:28 -0200

    transacao

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -9,6 +9,26 @@ public class Main {
         // faz a conexão ao banco
         PostgreSQL conexao = (new Conexao("nome do sistema")).getConexao();
         
+        /*
+        try {
+            // inicio a transação
+            conexao.setAutoCommit(false);
+            
+            conexao.execute("delete from imagem where id_imagem = ?", new Object[]{10});
+            
+            // finalizo a transação
+            conexao.setAutoCommit(true);            
+        } catch (Exception e) {
+            // deu merda... faço o rollback e desativo a transação
+            conexao.rollback();
+            
+            conexao.setAutoCommit(true);
+            
+            System.out.println(e.getMessage());
+        }
+        */
+        
+        
         ResultSet rs = conexao.query("select current_timestamp", new Object[0]);
         
         if(rs.next()) {
